48bc04f6f193305ff29556bb0e555c7aa45a66c2
Icez <kpto.isaac@gmail.com>
2024-06-03 16:04:45 +0800

    refactor code for better aliignment and use functional component.

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,23 +2,17 @@ import React from "react";
 import Card from "./Card";
 
 const CardList = ({ robots }) => {
-	const cards = robots.map(
-		(robot, i) => {
-			return (
-				<Card
-					key={i}
-					id={robot.id}
-					name={robot.name}
-					email={robot.email}
-				/>
-			);
-		}
-	);
-	return (
-		<React.Fragment>
-			{cards}
-		</React.Fragment>
-	);
+	const cards = robots.map((robot, i) => {
+		return (
+			<Card
+				key={i}
+				id={robot.id}
+				name={robot.name}
+				email={robot.email}
+			/>
+		);
+	});
+	return <React.Fragment>{cards}</React.Fragment>;
 };
 
 export default CardList;
--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,6 +1,4 @@
-import React, {
-	Component,
-} from "react";
+import React, { Component } from "react";
 
 class ErrorBoundary extends Component {
 	constructor(props) {
@@ -16,11 +14,7 @@ class ErrorBoundary extends Component {
 
 	render() {
 		if (this.state.hasError) {
-			return (
-				<h1>
-					Oooooops. That is not good
-				</h1>
-			);
+			return <h1>Oooooops. That is not good</h1>;
 		}
 		return this.props.children;
 	}
